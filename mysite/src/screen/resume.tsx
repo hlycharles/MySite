2695f4eb0d21c36dd8cd1f9965e45f1e91989525
hlycharles <hly.charles@gmail.com>
2017-06-26 22:24:41 -0700

    Denote resume path using a variable

--- mysite/src/screen/resume.tsx
+++ mysite/src/screen/resume.tsx
@@ -7,6 +7,8 @@ import "./style.scss";
 
 export default function ResumeScreen() {
 
+    const docPath = "../../asset/doc/resume.svg";
+
     return (
         <div>
             <div className="header-container">
@@ -14,7 +16,7 @@ export default function ResumeScreen() {
                 <Navigator panel={PANEL.RESUME} />
             </div>
             <div>
-                <object type="image/svg+xml" data="../../asset/doc/resume.svg">
+                <object type="image/svg+xml" data={docPath}>
                     Bad Browser
                 </object>
             </div>
