91deb111aab4150f755193cd3c9987cd16f8c94c
hlycharles <hly.charles@gmail.com>
2017-07-04 17:17:01 -0700

    Setup Me screen

--- mysite/src/screen/me.tsx
+++ mysite/src/screen/me.tsx
@@ -18,22 +18,124 @@ export default class MeScreen extends
                     <Navigator panel={PANEL.ME} />
                 </div>
                 <div className="content-container">
-                    {this._renderRowOne()}
+                    {this._renderEducation()}
+                </div>
+                <div className="content-container">
+                    {this._renderOffwork()}
+                </div>
+                <div className="content-container">
+                    {this._renderExperience()}
                 </div>
             </div>
         );
     }
 
-    private _renderRowOne(): React.ReactNode {
-        const resumeProp: BulletinProps = {
-            class: "resume",
-            cover: false,
-            detailedView: <p>Hi</p>,
-        };
+    private _renderEducation(): React.ReactNode {
         return (
             <BulletinBoard
-                bulletinProps={[resumeProp]}
+                title="Education"
+                bulletinProps={[
+                    this._renderCMU(),
+                    this._renderMajor(),
+                ]}
             />
         );
+    }
+
+    private _renderOffwork(): React.ReactNode {
+        return (
+            <BulletinBoard
+                title="Off-work"
+                bulletinProps = {[
+                    this._renderMusic(),
+                    this._renderReading(),
+                    this._renderArsenal(),
+                ]}
+            />
+        );
+    }
+
+    private _renderExperience(): React.ReactNode {
+        return (
+            <BulletinBoard
+                title="Experiences"
+                bulletinProps = {[
+                    this._renderNavlab(),
+                    this._renderRemitly(),
+                ]}
+            />
+        );
+    }
+
+    // bulletin renderers
+
+    private _renderCMU(): BulletinProps {
+        const cmuProps: BulletinProps = {
+            class: "cmu",
+            cover: false,
+            headerText: "CMU",
+            img: "../../asset/img/cmu_icon.png",
+        };
+        return cmuProps;
+    }
+
+    private _renderMajor(): BulletinProps {
+        const majorProps: BulletinProps = {
+            class: "major",
+            cover: false,
+            headerText: "ECE CS",
+            img: "../../asset/img/cs_icon.png",
+        };
+        return majorProps;
+    }
+
+    private _renderMusic(): BulletinProps {
+        const musicProps: BulletinProps = {
+            class: "music",
+            cover: false,
+            headerText: "Music",
+            img: "../../asset/img/music_icon.png",
+        };
+        return musicProps;
+    }
+
+    private _renderReading(): BulletinProps {
+        const readingProps: BulletinProps = {
+            class: "reading",
+            cover: false,
+            headerText: "Reading",
+            img: "../../asset/img/reading_icon.png",
+        };
+        return readingProps;
+    }
+
+    private _renderArsenal(): BulletinProps {
+        const arsenalProps: BulletinProps = {
+            class: "arsenal",
+            cover: false,
+            headerText: "Arsenal",
+            img: "../../asset/img/arsenal_icon.png",
+        };
+        return arsenalProps;
+    }
+
+    private _renderNavlab(): BulletinProps {
+        const remitlyProps: BulletinProps = {
+            class: "navlab",
+            cover: false,
+            headerText: "Navlab CMU",
+            img: "../../asset/img/navlab_icon.png",
+        };
+        return remitlyProps;
+    }
+
+    private _renderRemitly(): BulletinProps {
+        const remitlyProps: BulletinProps = {
+            class: "remitly",
+            cover: false,
+            headerText: "Remitly",
+            img: "../../asset/img/remitly_icon.png",
+        };
+        return remitlyProps;
     }
 }
