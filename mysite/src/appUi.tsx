8b157312535fdd288be9f4edce19eee84f18411f
hlycharles <hly.charles@gmail.com>
2017-07-11 20:48:26 -0700

    Use browser history

--- mysite/src/appUi.tsx
+++ mysite/src/appUi.tsx
@@ -1,6 +1,6 @@
 import autobind from "autobind-decorator";
 import * as H from "history";
-import { createHashHistory } from "history";
+import { createBrowserHistory } from "history";
 import PropTypes from "prop-types";
 import * as React from "react";
 import { Route, Router, Switch } from "react-router";
@@ -35,9 +35,9 @@ export default class AppUi extends
     }
 
     componentWillMount() {
-        this.history = createHashHistory({
+        this.history = createBrowserHistory({
             basename: "",
-            hashType: "noslash",
+            forceRefresh: false,
         });
     }
 
