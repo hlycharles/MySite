dea80533d1023eb1fe728597170c4f78640bb37f
hlycharles <hly.charles@gmail.com>
2017-07-11 20:53:23 -0700

    Add script for production build and upload

---
+++ mysite/prod.sh
@@ -0,0 +1,3 @@
+webpack -p
+scp -i ../../SiteKey.pem -r build ubuntu@ec2-52-53-239-1.us-west-1.compute.amazonaws.com:
+scp -i ../../SiteKey.pem -r asset ubuntu@ec2-52-53-239-1.us-west-1.compute.amazonaws.com:
