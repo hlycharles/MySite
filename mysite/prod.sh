71e9132a17de2e4a8a79a104a27a364bf09b89af
hlycharles <hly.charles@gmail.com>
2017-07-30 19:16:30 -0700

    Deploy with server hosting Instagram photos

--- mysite/prod.sh
+++ mysite/prod.sh
@@ -1,3 +1,6 @@
+git checkout insta
+git merge -
 webpack -p
 scp -i ../../SiteKey.pem -r build ubuntu@ec2-52-53-239-1.us-west-1.compute.amazonaws.com:
 scp -i ../../SiteKey.pem -r asset ubuntu@ec2-52-53-239-1.us-west-1.compute.amazonaws.com:
+git checkout -
